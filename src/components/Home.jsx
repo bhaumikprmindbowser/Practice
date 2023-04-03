2395842094b0f5759e13c8f63d6136465c964cfd
bhaumik p mindbowser <bhaumik.parmar@mindbowser.com>
2023-04-03 19:14:56 +0530

    check husky

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
 
 function Home() {
   return (
